3d91158bf20d2d9d420be5fecfed50ea27833e78
Miraz <miraz.miraj@gmail.com>
2025-10-25 11:48:56 +0600

    update admin

--- backend/accsoft/accsoft/urls.py
+++ backend/accsoft/accsoft/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from accounts.views import AccountViewSet, JournalEntryViewSet
@@ -24,6 +25,7 @@ router.register(r'accounts', AccountViewSet)
 router.register(r'journal-entries', JournalEntryViewSet)
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
